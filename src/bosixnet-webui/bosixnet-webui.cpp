babe083a4f317a0eba0a47e1d4f0440631daa62c
Boris Pek <tehnick-8@yandex.ru>
2016-10-06 23:45:39 +0300

    Update documentation.

--- src/bosixnet-webui/bosixnet-webui.cpp
+++ src/bosixnet-webui/bosixnet-webui.cpp
@@ -1,6 +1,6 @@
 /*****************************************************************************
  *                                                                           *
- *  Copyright (c) 2013-2015 Boris Pek <tehnick-8@mail.ru>                    *
+ *  Copyright (c) 2013-2016 Boris Pek <tehnick-8@mail.ru>                    *
  *                                                                           *
  *  Permission is hereby granted, free of charge, to any person obtaining    *
  *  a copy of this software and associated documentation files (the          *
